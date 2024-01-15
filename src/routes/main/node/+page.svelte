526b1ad5846487dd85f63de81089075d7d6d91b6
code0xff <ian.jungyong.um@gmail.com>
2024-01-15 23:16:50 +0900

    feat: Add cpu and memory check

--- src/routes/main/node/+page.svelte
+++ src/routes/main/node/+page.svelte
@@ -1,18 +1,61 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
 	import { Button } from '$lib/components/ui/button';
-	import { Coins, Cpu, Dumbbell, Package } from 'lucide-svelte';
+
+	import { AudioWaveform, Coins, Cpu, Dumbbell, MemoryStick, Package } from 'lucide-svelte';
+	import { invoke } from '@tauri-apps/api/tauri';
 
 	let nodeOn: boolean = false;
+	let cpuUsage: number = 0.0;
+	let memory: number = 0;
 
-	function startNode() {
+	let jobId: number = 0;
+
+	async function startNode() {
 		nodeOn = !nodeOn;
+
+		if (nodeOn) {
+			jobId = setInterval(() => {
+				invoke('check_status', { pid: 0 })
+					.then((status: any) => {
+						cpuUsage = status.cpu_usage;
+						memory = status.memory;
+					})
+					.finally(console.error);
+			}, 1000);
+		} else {
+			clearInterval(jobId);
+		}
 	}
 </script>
 
 <div>
 	<div class="w-full">
-		<Button class="w-full" variant={nodeOn ? "default" : "outline"} on:click={startNode}>{nodeOn ? "Node Stop" :  "Node Start"}</Button>
+		<Button class="w-full" variant={nodeOn ? 'default' : 'outline'} on:click={startNode}
+			>{nodeOn ? 'Node Stop' : 'Node Start'}</Button
+		>
+	</div>
+	<div class="grid gap-4 pt-4 md:grid-cols-1 lg:grid-cols-2">
+		<Card.Root>
+			<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+				<Card.Title class="text-sm font-medium">CPU usage</Card.Title>
+				<Cpu class="h-4 w-4 text-muted-foreground" />
+			</Card.Header>
+			<Card.Content>
+				<div class="text-2xl font-bold">{cpuUsage}</div>
+				<p class="text-xs text-muted-foreground"></p>
+			</Card.Content>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+				<Card.Title class="text-sm font-medium">Memory</Card.Title>
+				<MemoryStick class="h-4 w-4 text-muted-foreground" />
+			</Card.Header>
+			<Card.Content>
+				<div class="text-2xl font-bold">{memory}</div>
+				<p class="text-xs text-muted-foreground"></p>
+			</Card.Content>
+		</Card.Root>
 	</div>
 	<div class="grid gap-4 pt-4 md:grid-cols-2 lg:grid-cols-4">
 		<Card.Root>
@@ -28,7 +71,7 @@
 		<Card.Root>
 			<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
 				<Card.Title class="text-sm font-medium">My hashrate</Card.Title>
-				<Cpu class="h-4 w-4 text-muted-foreground" />
+				<AudioWaveform class="h-4 w-4 text-muted-foreground" />
 			</Card.Header>
 			<Card.Content>
 				<div class="text-2xl font-bold">19.00 KH/s</div>
